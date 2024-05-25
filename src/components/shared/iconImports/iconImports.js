3d888dae0fa41e3fe090be49d24726c06b50c627
Anton Bazyshyn <maybebazishin@gmail.com>
2024-05-25 08:51:07 +0300

    template

--- src/components/shared/iconImports/iconImports.js
+++
@@ -1,5 +0,0 @@
-import { FiGithub } from 'react-icons/fi'
-import { TbBrandLinkedin } from 'react-icons/tb'
-import { HiOutlineExternalLink } from 'react-icons/hi'
-
-export { FiGithub, TbBrandLinkedin, HiOutlineExternalLink }
