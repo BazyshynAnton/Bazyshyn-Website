bb1b3d16c3eed951d569675d54bb5d65702ce478
Anton Bazyshyn <maybebazishin@gmail.com>
2024-05-26 19:21:15 +0300

    add bg and intro-container

--- src/components/homePage/HomePage.tsx
+++ src/components/homePage/HomePage.tsx
@@ -1,5 +1,15 @@
 import styles from '../../styles/homePage/HomePage.module.css'
 
 export default function HomePage() {
-  return <div className={styles.container}>asdfasdf</div>
+  return (
+    <>
+      <div className={styles.homePageBackground}></div>
+      <div className="main-container">
+        <div className={styles.introduceHelperContainer}>
+          <div className={styles.introduceContainer}></div>
+        </div>
+        <div className={styles.introduceContainer}></div>
+      </div>
+    </>
+  )
 }
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -4,9 +4,9 @@ import { Outlet } from 'react-router'
 
 export default function MainLayout() {
   return (
-    <div className="main-container">
+    <>
       <Header />
       <Outlet />
-    </div>
+    </>
   )
 }
