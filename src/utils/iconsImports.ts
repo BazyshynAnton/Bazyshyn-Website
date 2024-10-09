81b1196fcb9a8aca8ef0417f0f15782ce62a56c7
BazyshynAnton <maybebazishin@gmail.com>
2024-10-09 18:17:34 +0300

    fonts issue

--- src/utils/iconsImports.ts
+++ src/utils/iconsImports.ts
@@ -1,12 +1,12 @@
-import { SiLeetcode } from 'react-icons/si'
-import { GrGithub } from 'react-icons/gr'
-import { FaLinkedin } from 'react-icons/fa'
-import { IoLink } from 'react-icons/io5'
-import { IoIosArrowDown } from 'react-icons/io'
-import { IoMdMenu } from 'react-icons/io'
-import { IoMdMap } from 'react-icons/io'
-import { MdOutlineEmail } from 'react-icons/md'
-import { MdLaunch } from 'react-icons/md'
+import { SiLeetcode } from "react-icons/si"
+import { GrGithub } from "react-icons/gr"
+import { FaLinkedin } from "react-icons/fa"
+import { IoLink } from "react-icons/io5"
+import { IoIosArrowDown } from "react-icons/io"
+import { IoMdMenu } from "react-icons/io"
+import { IoMdMap } from "react-icons/io"
+import { MdOutlineEmail } from "react-icons/md"
+import { MdLaunch } from "react-icons/md"
 
 export {
   SiLeetcode,
